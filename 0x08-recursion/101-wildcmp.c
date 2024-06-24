50a709e1f260643957b1ec10a784547cbf2072be
dantel <rowlanddantel@gmail.com>
2024-06-24 09:37:17 -0800

    complied to betty style of coding

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,34 +1,32 @@
 #include "main.h"
 
 /**
- *  * wildcmp - Compares two strings, considering wildcard character '*'
- *   * @s1: The first string
- *    * @s2: The second string (can contain wildcard '*')
- *     *
- *      * Return: 1 if the strings can be considered identical, 0 otherwise
- *       */
+ * * wildcmp - Compares two strings, considering wildcard character '*'
+ * * @s1: The first string
+ * * @s2: The second string (can contain wildcard '*')
+ * *
+ * * Return: 1 if the strings can be considered identical, 0 otherwise
+*/
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-    {
-        return (1);
-    }
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-        {
-            return (wildcmp(s1, s2 + 1));
-        }
-        if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
-        {
-            return (1);
-        }
-    }
-    if (*s1 == *s2)
-    {
-        return (wildcmp(s1 + 1, s2 + 1));
-    }
-    return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+		{
+			return (1);
+		}
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
-
-
