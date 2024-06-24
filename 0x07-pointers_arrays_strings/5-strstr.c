113d5d33f1ecbc456e4508b2172143753c44e29b
dantel <rowlanddantel@gmail.com>
2024-06-24 15:14:07 -0800

    complied to betty style of coding

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,35 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * _strstr - locates a substring
- * @haystack: string to search
- * @needle: substring to locate
+ * _strstr - locates a substring.
+ * @haystack: the string to be searched.
+ * @needle: the substring to locate.
  *
- * Return: pointer to beginning
- *of located substring, or NULL if not found
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
 */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
-	{
-		char *h = haystack;
-		char *n = needle;
+	char *h, *n;
 
-		while (*n && *h == *n)
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*h != '\0' && *n != '\0' && *h == *n)
 		{
 			h++;
 			n++;
 		}
-		if (!*n)
+		if (*n == '\0')
+		{
 			return (haystack);
+		}
 		haystack++;
 	}
 	return (NULL);
