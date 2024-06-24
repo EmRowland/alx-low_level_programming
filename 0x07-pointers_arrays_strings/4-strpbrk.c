c3f46236856ef31eccf86b21fcac29e5b6688d91
dantel <rowlanddantel@gmail.com>
2024-06-24 10:11:27 -0800

    complied to betty style of coding

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,24 @@
 #include "main.h"
-#include "tasks/4-strpbrk.c"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: set of bytes to search for
+ * 
+ * Return: pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    while (*s)
+    {
+        char *a = accept;
 
+        while (*a)
+        {
+            if (*s == *a)
+                return s;
+            a++;
+        }
+        s++;
+    }
+    return NULL;
+}
